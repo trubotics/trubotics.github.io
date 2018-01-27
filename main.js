a3a2f0aabd2766760b2aefac23ec70e27a0940f2
jakexia72 <jakexia72@hotmail.com>
2018-01-26 22:07:46 -0500

    test

--- main.js
+++ main.js
@@ -7,12 +7,13 @@ var main = function () {
     
     //setting the jumbotron text logo
     if (window.innerWidth < 768){
-        $('#jumbotronFull').css("background-image", "url(Img/jumboVertical.jpg)")
-        $('#textLogo').css("width", window.innerWidth * 0.75)
-        $('#textLogo').attr("src", "Img/textLogoVertical.png")
+        $('#jumbotronFull').css("background-image", "url(Img/jumboVertical.jpg)");
+        $('#textLogo').css("width", window.innerWidth * 0.75);
+        $('#textLogo').attr("src", "Img/textLogoVertical.png");
         
     } else {
-            $('#textLogo').css("width", window.innerWidth * 0.6)  
+            $('#textLogo').css("width", window.innerWidth * 0.6);
+            $('#jumbotronFull').css("background-attachment", "fixed");
     }
     
 
